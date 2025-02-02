0a20c64ce385c929d827173a27b871a7d8ed4cb6
Churakov Pavel <131313pasha@gmail.com>
2025-02-02 14:07:45 +0300

    fix dev/prod

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,23 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: process.env.VITE_BASE_URL || "/",
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      api: path.resolve(__dirname, "./src/api"),
-      components: path.resolve(__dirname, "./src/components"),
-      "components-shadcn": path.resolve(__dirname, "./src/components-shadcn"),
-      hooks: path.resolve(__dirname, "./src/hooks"),
-      pages: path.resolve(__dirname, "./src/pages"),
-      types: path.resolve(__dirname, "./src/types"),
+export default defineConfig(({ mode }) => {
+  // Проверка режима
+  const isProduction = mode === "production";
+
+  return {
+    base: isProduction ? "/auth-app/" : "/",
+    // другие настройки для продакшн и разработки
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        api: path.resolve(__dirname, "./src/api"),
+        components: path.resolve(__dirname, "./src/components"),
+        "components-shadcn": path.resolve(__dirname, "./src/components-shadcn"),
+        hooks: path.resolve(__dirname, "./src/hooks"),
+        pages: path.resolve(__dirname, "./src/pages"),
+        types: path.resolve(__dirname, "./src/types"),
+      },
     },
-  },
+  };
 });
