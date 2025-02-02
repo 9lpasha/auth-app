421511f4b74e72fe44d9a18118fcd59cffd1693c
Churakov Pavel <131313pasha@gmail.com>
2025-02-02 14:13:17 +0300

    fix router

--- src/app/app-router.tsx
+++ src/app/app-router.tsx
@@ -3,7 +3,7 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { AuthPage, ProfilePage } from "pages";
 
 export const AppRouter = () => (
-  <Router>
+  <Router basename={location.hostname === "localhost" ? "/" : "/auth-app"}>
     <Routes>
       <Route path="/" element={<AuthPage />} />
       <Route path="/profile" element={<ProfilePage />} />
